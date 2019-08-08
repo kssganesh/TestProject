df5f37553c29f27df06ac8a7341c352ccf69a403
Ganesh <Ganesh@Ganesh-PC>
2019-08-08 22:11:53 +0530

    commit

--- portalpages/src/test/java/OnlinePortal/portalpages/FbHomePage.java
+++ portalpages/src/test/java/OnlinePortal/portalpages/FbHomePage.java
@@ -8,12 +8,9 @@ import org.openqa.selenium.support.How;
 public class FbHomePage {
  
  WebDriver driver;
- 
         public FbHomePage(WebDriver driver){ 
                 this.driver=driver; 
         }
- 
-        
         //Using FindBy for locating elements
  @FindBy(how=How.XPATH, using="//div[text()='Account Settings']") WebElement profileDropdown;
  @FindBy(how=How.XPATH, using="//text()[.='Log Out']/ancestor::span[1]") WebElement logoutLink;
