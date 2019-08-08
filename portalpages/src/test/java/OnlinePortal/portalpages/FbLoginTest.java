dd3d1ce2df03f87d85e9d9d049a0e9234cb9194f
Ganesh <Ganesh@Ganesh-PC>
2019-08-08 21:10:54 +0530

    First Commit

---
+++ portalpages/src/test/java/OnlinePortal/portalpages/FbLoginTest.java
@@ -0,0 +1,24 @@
+package OnlinePortal.portalpages;
+
+
+
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Test;
+
+public class FbLoginTest extends TestBase{
+	
+	@Test
+	public void init() throws Exception{
+
+			//driver.get("https://www.facebook.com");
+			FbLoginPage loginpage = PageFactory.initElements(driver, FbLoginPage.class);
+			loginpage.setEmail("");
+			loginpage.setPassword("");
+			loginpage.clickOnLoginButton();
+			
+			FbHomePage homepage = PageFactory.initElements(driver, FbHomePage.class);
+			homepage.clickOnProfileDropdown();
+			homepage.verifyLoggedInUserNameText();
+			homepage.clickOnLogoutLink();	
+		}
+}
